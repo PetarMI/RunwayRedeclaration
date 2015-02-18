01bbde1599fabf042ef6373c9078c426ef8ff746
PetarMI <pmi1u12@soton.ac.uk>
2015-02-18 15:45:24 +0000

    Got back Andrei's runway

--- src/main/java/Model/Runway.java
+++ src/main/java/Model/Runway.java
@@ -1,10 +1,55 @@
 package Model;
 import java.awt.geom.Point2D;
 
-public class Runway {
-
-
+public class Runway
+{
     private String runwayId;
+    private int tora;
+    private int asda;
+    private int toda;
+    private int lda;
+    private String orientation;
+    private int position;
+
+    public Runway(String runwayId, int tora, int asda, int toda, int lda, String orientation, int position) {
+        this.runwayId = runwayId;
+        this.tora = tora;
+        this.asda = asda;
+        this.toda = toda;
+        this.lda = lda;
+        this.orientation = orientation;
+        this.position = position;
+    }
+
+    public String getRunwayId() {
+        return runwayId;
+    }
+
+    public int getTora() {
+        return tora;
+    }
+
+    public int getAsda() {
+        return asda;
+    }
+
+    public int getToda() {
+        return toda;
+    }
+
+    public int getLda() {
+        return lda;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    /*private String runwayId;
     // Unsure whether we need to store these in runway, could be held in the lower strip and called from there
     private int degree;
     private String position;
@@ -46,5 +91,5 @@ public class Runway {
 
     public String getPosition() {
         return position;
-    }
+    }*/
 }
