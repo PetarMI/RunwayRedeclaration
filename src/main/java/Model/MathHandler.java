65fea897185104943c675f38f6f72cf131cfa402
PetarMI <pmi1u12@soton.ac.uk>
2015-02-22 15:49:30 +0000

    Started MathHandler and made changes to Runway class

---
+++ src/main/java/Model/MathHandler.java
@@ -0,0 +1,14 @@
+package Model;
+
+/**
+
+ */
+public class MathHandler
+{
+    public MathHandler() {  };
+
+    /*public void recalculateValues(Runway runway, int objHeight, ){
+
+    }*/
+
+}
--- src/main/java/Model/Runway.java
+++ src/main/java/Model/Runway.java
@@ -6,10 +6,14 @@ public class Runway
     private String runwayId;
     private Strip strip1;
     private Strip strip2;
+    private Obstacle obstacle;
+    private int obstaclePosition;
 
     public Runway(String runwayId, Strip strip1, Strip strip2) {
+        this.runwayId = runwayId;
         this.strip1 = strip1;
         this.strip2 = strip2;
+        this.obstacle = null;
     }
 
     public String getRunwayId() {
@@ -19,6 +23,11 @@ public class Runway
     public Strip getStrip1() { return strip1; }
 
     public Strip getStrip2() { return strip2; }
+
+    public void addObstacle(Obstacle obst, int pos){
+        this.obstacle = obst;
+        this.obstaclePosition = pos;
+    }
 
     @Override
     public String toString() {
